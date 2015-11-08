91277404e79c2b7667afaf9d0001e787432b3bac
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-11-08 18:33:53 +0900

    Add aws-sdk initializer

---
+++ config/initializers/aws.rb
@@ -0,0 +1,6 @@
+# see http://docs.aws.amazon.com/sdkforruby/api/index.html#Configuration
+
+#Aws.config.update({
+#  region: 'us-west-2',
+#  credentials: Aws::Credentials.new('akid', 'secret'),
+#})
