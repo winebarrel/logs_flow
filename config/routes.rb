47bc7fd15dccb431a36a768efbc47f08cecaf863
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-11-08 18:20:06 +0900

    Add streams#index/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'streams/index'
+
+  get 'streams/show'
+
   resources :groups, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
